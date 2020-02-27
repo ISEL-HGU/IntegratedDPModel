fb52ca9e491913c3e58e0b66a37da75501e8ea13
Eunji Won <21500420@handong.edu>
2020-02-27 11:50:50 +0900

    add WFS shell script

---
+++ WFS_exp.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# type3
+#./EJTool -s /home/eunjiwon/Git/EJTool/fs_cross_validation_data/ -t 3 -c /home/eunjiwon/Git/EJTool/multi_results/t3_LR_result.csv -i 10 -f 10 -m weka.classifiers.functions.Logistic -d 1 -p 20
+#./EJTool -s /home/eunjiwon/Git/EJTool/fs_cross_validation_data/ -t 3 -c /home/eunjiwon/Git/EJTool/multi_results/t3_RF_result.csv -i 10 -f 10 -m weka.classifiers.trees.RandomForest -d 1 -p 25
+./EJTool -s /home/eunjiwon/Git/EJTool/fs_cross_validation_data/ -t 3 -c /home/eunjiwon/Git/EJTool/multi_results/t3_DT_result.csv -i 10 -f 10 -m weka.classifiers.trees.J48 -d 1 -p 25
