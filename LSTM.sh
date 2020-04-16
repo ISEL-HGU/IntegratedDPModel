e7813ff7da2777232093f7bac6f6fe55bd1d756b
Eunji Won <21500420@handong.edu>
2020-04-16 16:03:42 +0900

    add LSTM.sh

---
+++ LSTM.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# type5
+./EJTool -s /home/eunjiwon/Git/EJTool/LSTM_cross_validation_data/ -t 5 -c /home/eunjiwon/Git/EJTool/multi_results/LSTM_LR_result.csv -i 100 -f 10 -m weka.classifiers.functions.Logistic -d 3 -p 20
