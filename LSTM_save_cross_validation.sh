be5f6472440c2e81f3558b2b3dcdb22374562513
Eunji Won <21500420@handong.edu>
2020-04-29 13:10:01 +0900

    update LSTM_save_cross_validation script

--- LSTM_save_cross_validation.sh
+++ LSTM_save_cross_validation.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-for projectname in "camel" "eagle" "groovy" "jena" "juddi" "metamodel" "nutch";
+for projectname in "ace" "ant-ivy" "bigtop" "bval" "camel" "cayenne" "cordova-android" "creadur-rat" "crunch" "deltaspike" "gora" "groovy" "guacamole-client" "incubator-hivemall";
 do
-./EJTool -s /home/eunjiwon/Git/EJTool/LSTM_origin_data_epoch_20_batchsize_32/${projectname}_Add_LSTM_metric.arff -d /home/eunjiwon/Git/EJTool/LSTM_cross_validation_data -i 100 -f 10 -a ${projectname}_Add_LSTM_metric.arff
+./EJTool -s /home/eunjiwon/Git/EJTool/LSTM_origin_data_epoch_20_batchsize_32/${projectname}_developer.arff -d /home/eunjiwon/Git/EJTool/LSTM_cross_validation_data -i 100 -f 10 -a ${projectname}_developer.arff
+
+./EJTool -s /home/eunjiwon/Git/EJTool/LSTM_origin_data_epoch_20_batchsize_32/${projectname}_Concat_Single_LSTM_Metric.arff -d /home/eunjiwon/Git/EJTool/LSTM_cross_validation_data -i 100 -f 10 -a ${projectname}_Concat_Single_LSTM_Metric.arff
+
+./EJTool -s /home/eunjiwon/Git/EJTool/LSTM_origin_data_epoch_20_batchsize_32/${projectname}_Line_Single_LSTM_Metric.arff -d /home/eunjiwon/Git/EJTool/LSTM_cross_validation_data -i 100 -f 10 -a ${projectname}_Line_Single_LSTM_Metric.arff
+
+./EJTool -s /home/eunjiwon/Git/EJTool/LSTM_origin_data_epoch_20_batchsize_32/${projectname}_Master_NGLP_Metric.arff -d /home/eunjiwon/Git/EJTool/LSTM_cross_validation_data -i 100 -f 10 -a ${projectname}_Master_NGLP_Metric.arff
+
+./EJTool -s /home/eunjiwon/Git/EJTool/LSTM_origin_data_epoch_20_batchsize_32/${projectname}_Single_NGLP_Metric.arff -d /home/eunjiwon/Git/EJTool/LSTM_cross_validation_data -i 100 -f 10 -a ${projectname}_Single_NGLP_Metric.arff
+
 done
+
+
+
+
